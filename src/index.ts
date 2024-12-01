c2eff46fc09e3b62ebf75769aa293b05c6603351
PuttTim <yannaputt@gmail.com>
2024-12-01 14:03:35 +0800

    ✨ Added basic functionality of cron messages

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,31 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	BOT_TOKEN: string;
+	CHAT_ID: string;
+	TEST_CHAT_ID: string;
+}
+
+import messages from './messages.json';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
+		const date = new Date().getDate().toString() as keyof typeof messages;
+
+		// new fetch POST request
+		const response = await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				chat_id: env.CHAT_ID,
+				text: messages[date] + '\n\n' + `https://adventofcode.com/2024/day/${date}`,
+			}),
+		});
+
+		// log response
+		console.log(await response.json());
 	},
 } satisfies ExportedHandler<Env>;
